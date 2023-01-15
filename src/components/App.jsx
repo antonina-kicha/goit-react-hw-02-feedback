922af7404289bccb3b85cc309b0664f57978c0b5
Antonina Kicha <antonina.kicha@gmail.com>
2023-01-15 22:47:21 +0200

    Добавляет стартовые файлы проекта_feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,25 @@
-import { Profile } from './Profile/Profile';
+import { Component } from 'react';
+
+import { Feedback } from './Feedback/Feedback';
 import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
-
-
-import user from '../user.json';
-import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
-
-
-
-export const App = () => {
-  return (
+export class App extends Component {
+  state =  {
+      good: 0,
+      neutral: 0,
+      bad: 0
+  }
+  render() {
+      return (
     <>
-      <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-
-    </>
-    
-    
+      <Feedback/>
+      <Statistics/>
+    </> 
   );
-};
+    
+  }
+}
+
+
 
 
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+export const Feedback = () => {
+    return (
+        <>
+            <h2>Please leave feedback</h2>
+            <button type='button'>Good</button>
+            <button type='button'>Neutral</button>
+            <button type='button'>Bad</button>
+        </>
+        
+    )
+}
